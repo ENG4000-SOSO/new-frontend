ee28de03c2b5ba09fee4850415c24e4501732ea2
Daniel Di Giovanni <dannyjdigio@gmail.com>
2025-04-24 14:53:04 -0400

    Removed port from production api url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: "http://localhost:8000"
   // baseURL: "http://44.195.89.245:8000"
-  baseURL: "https://soso.danieldigiovanni.com:8000"
+  baseURL: "https://soso.danieldigiovanni.com"
 });
 
 api.interceptors.request.use(
