664f6f1f5be2849264e7c186531193ea37e78b81
Daniel Di Giovanni <dannyjdigio@gmail.com>
2025-04-24 03:57:19 -0400

    Added IP of backend server

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000"
+  // baseURL: "http://localhost:8000"
+  baseURL: "http://44.195.89.245:8000"
 });
 
 api.interceptors.request.use(
